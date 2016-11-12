466da3e0c2460ecad887cad74b680ab96f148bf8
Devwrat More <dmm2228@columbia.edu>
2016-11-12 13:35:42 -0500

    Sensor reading in thread done.

---
+++ navigator/demoSensor.py
@@ -0,0 +1,29 @@
+import Navigator
+import time
+import json
+
+from threading import Thread
+import random
+
+#nav = Navigator.Navigator()
+#nav.startSafe()
+
+def getFakeSensorData():
+    sensorData = {}
+    sensorData['angle'] = random.randint(0, 360)
+    sensorData['distance'] = random.randint(0, 100)
+    return sensorData
+
+def readSensors(param):
+    while True:
+        #sensorData = nav.readSensorsData()
+        sensorData = getFakeSensorData()
+        print json.dumps(sensorData, indent=4, sort_keys=False)
+        time.sleep(1)
+
+thread = Thread(target=readSensors, args=('someParam', ))
+thread.start()
+thread.join()
+
+print 'thread exiting'
+
