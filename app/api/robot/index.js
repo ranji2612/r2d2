ed4a3c1cbdcb539d9175c236a187b55d63acb8c7
Somdeep <sd2988@columbia.edu>
2016-11-12 02:35:49 -0500

    untested routes

--- app/api/robot/index.js
+++ app/api/robot/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express.Router();
+var create = require('create2'),chalk = require('chalk');
+var robot, input = 1;
 
 // Routes for /api/robot
 app.get('/start', function(req, res){
+  create.inputMode = 2;
+  create.prompt(function(p){
+    create.open(p,setup)});
   res.json({'status':200, 'action':'start'});
 });
 
@@ -11,10 +16,18 @@ app.get('/stop', function(req, res){
 });
 
 app.post('/drive', function(req, res){
+  robot.start();
+  robot.drive(100,200);
   var velocity = req.body.velocity,
       radius   = req.body.radius;
   console.log(velocity, radius);
   res.json({'status':200, 'action':'drive'});
 });
+
+
+function setup(r) {
+  robot = r;
+  }
+
 
 module.exports = app;
