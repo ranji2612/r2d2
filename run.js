f33aae5465309382a7dc2e5e246e278541a4e67c
Siri Haricharan <siri47@gmail.com>
2016-11-12 00:39:21 -0500

    passive mode, clean working

---
+++ run.js
@@ -0,0 +1,20 @@
+var create = require('create2'),chalk = require('chalk');
+var robot, input = 1;
+function main(r) {robot = r;
+                  //robot.reset();
+                  robot.start();
+                  
+                 //robot.safe();
+                 robot.clean();
+                 //robot.drive(100,2000);
+                  //robot.reset();
+//                setTimeout(function(){
+//                    robot.stop();
+//                }, 10000);
+                //robot.stop();
+                 }
+
+function start2() {create.inputMode = 2; create.prompt(function(p){create.open(p,main)});}
+start2();
+
+
